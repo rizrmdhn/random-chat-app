bae614254e762abf03fa41d9aa563e5b35adfbbb
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-02-01 14:41:30 +0800

    add event types and interfaces for channel and message events

--- src/types/event.types.ts
+++ src/types/event.types.ts
@@ -2,6 +2,11 @@ export const EVENT_TYPES = [
   "user.created",
   "user.updated",
   "user.deleted",
+  "channel.created",
+  "message.sent",
+  "message.received",
+  "message.deleted",
+  "message.updated",
 ] as const;
 
 export type EventType = (typeof EVENT_TYPES)[number];
@@ -17,8 +22,25 @@ export interface UserEvent extends BaseEvent {
   name: string;
 }
 
+export interface ChannelEvent extends BaseEvent {
+  channelId: string;
+  name: string;
+}
+
+export interface MessageEvent extends BaseEvent {
+  messageId: string;
+  channelId: string;
+  userId: string;
+  message: string;
+}
+
 export type EventMap = {
   "user.created": UserEvent;
   "user.updated": UserEvent;
   "user.deleted": Pick<UserEvent, "userId" | "id" | "timestamp">;
+  "channel.created": ChannelEvent;
+  "message.sent": MessageEvent;
+  "message.received": MessageEvent;
+  "message.deleted": Pick<MessageEvent, "messageId" | "id" | "timestamp">;
+  "message.updated": MessageEvent;
 };
